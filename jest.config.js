7b32c374b29ef0a619dc5180c541ac9eb7eff146
Guido Wagner <wagner.guido.5@gmail.com>
2025-03-27 00:30:34 -0300

    chore: project setup

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,17 @@
+/** @type {import('jest').Config} */
 module.exports = {
-    transform: {
+  transform: {
     '^.+\\.(t|j)sx?$': '@swc/jest',
-    },
-   }
-   
+  },
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  testEnvironment: 'node',
+  rootDir: './',
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  coverageReporters: ['json', 'text', 'lcov', 'clover'],
+  testMatch: ['**/*.test.ts'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  detectOpenHandles: true,
+};
